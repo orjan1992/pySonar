23e3c6b4d34ed7bb28bbaf29456445f3461c9eef
Ørjan Grefstad <orjangr@stud.ntnu.no>
2017-11-08 13:25:19 +0100

    Changed class layout

--- testZhouQt.py
+++ testZhouQt.py
@@ -10,41 +10,16 @@ class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.central_widget = QtGui.QStackedWidget()
-        self.login_widget = LoginWidget(self)
-        self.login_widget.button.clicked.connect(self.plotter)
+        self.login_widget = MainWidget(self)
         self.central_widget.addWidget(self.login_widget)
         self.setCentralWidget(self.central_widget)
 
-        self.threshold_box = self.login_widget.threshold_box
 
-
-
-    def plotter(self):
-        log = 'logs/360 degree scan harbour piles.V4LOG'
-        self.file = ReadLogFile(log)
-        self.O = OGrid(0.1, 20, 15, 0.5)
-        self.img = self.login_widget.img_item
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.updater)
-        self.timer.start(0)
-
-    def updater(self):
-        msg = self.file.readNextMsg()
-        if msg == -1:
-            self.timer.stop()
-        if msg != 0:
-            while msg.type != 2:
-                msg = self.file.readNextMsg()
-            self.O.autoUpdateZhou(msg, self.threshold_box.value())
-            self.img.setImage(self.O.getP().T)
-
-
-class LoginWidget(QtGui.QWidget):
+class MainWidget(QtGui.QWidget):
     def __init__(self, parent=None):
-        super(LoginWidget, self).__init__(parent)
+        super(MainWidget, self).__init__(parent)
         main_layout = QtGui.QHBoxLayout() # Main layout
         left_layout = QtGui.QVBoxLayout()
-
 
         graphics_view = pg.GraphicsLayoutWidget() # layout for holding graphics object
         view_box = pg.ViewBox(invertY=True) # making viewbox for the image, inverting y to make it right
@@ -63,9 +38,13 @@ class LoginWidget(QtGui.QWidget):
         self.threshold_box.setMaximum(255)
         self.threshold_box.setValue(60)
 
+        # Select file
+        self.select_file_button = QtGui.QPushButton('Select File')
+
+
         # Adding items
-        left_layout.addWidget(self.button)
         left_layout.addWidget(self.threshold_box)
+        left_layout.addWidget(self.button)
 
         main_layout.addLayout(left_layout)
         main_layout.addWidget(graphics_view)
@@ -73,6 +52,25 @@ class LoginWidget(QtGui.QWidget):
         view_box.addItem(self.img_item)
         graphics_view.addItem(view_box)
         self.setLayout(main_layout)
+        self.button.clicked.connect(self.plotter)
+
+    def plotter(self):
+        log = 'logs/360 degree scan harbour piles.V4LOG'
+        self.file = ReadLogFile(log)
+        self.O = OGrid(0.1, 20, 15, 0.5)
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.updater)
+        self.timer.start(0)
+
+    def updater(self):
+        msg = self.file.readNextMsg()
+        if msg == -1:
+            self.timer.stop()
+        if msg != 0:
+            while msg.type != 2:
+                msg = self.file.readNextMsg()
+            self.O.autoUpdateZhou(msg, self.threshold_box.value())
+            self.img_item.setImage(self.O.getP().T)
 
 
 if __name__ == '__main__':
