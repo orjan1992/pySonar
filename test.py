b7f5a6937ba6f4436e3a17e7af1510dc4bcfcad7
Ørjan Grefstad <orjangr@stud.ntnu.no>
2017-11-08 13:03:49 +0100

    Layouts and threshold selector works

--- test.py
+++ test.py
@@ -1,26 +1,2 @@
-from readLogFile.readCsvFile import ReadCsvFile
-# log = '/home/orjangr/Repos/pySonar/logs/UdpHubLog_4001_2017_11_02_09_00_03.csv'
-# file = ReadCsvFile(log, 4002, 13102)
-#
-# for i in range(0, 2000):
-#     msg = file.readNextMsg()
-#     if msg != -1 and msg != 0:
-#         print(msg.sensorStr)
-#     elif msg == -1:
-#         break
-# file.close()
-import math
-from readLogFile.oGrid import OGrid
-import matplotlib.pyplot as plt
-grid = OGrid(0.1, 10, 5, 0.5)
-step = 32*grid.GRAD2RAD
-cells = grid.sonarConeLookup(step, math.pi/4)
-grid.updateCells(cells, 1000)
-cells = grid.sonarConeLookup(step, -math.pi/4)
-grid.updateCells(cells, -1000)
-fig, ax = grid.showP()
-fig.show()
-# #plotting
-# fig, ax = plt.subplots()
-# ax.imshow(grid.oLog, extent=[0, 1, 0, 1])
-# plt.show()
+a = [[0.0, 1.0, 0.0], [0.0, 1.0, 1.0], [1.0, 1.0, 0.0], [1.0, 0.0, 0.0]]
+print(a[3, 2])
--- testZhouQt.py
+++ testZhouQt.py
@@ -3,6 +3,7 @@ import pyqtgraph as pg
 from readLogFile.oGrid import OGrid
 from readLogFile.sonarMsg import SonarMsg
 from readLogFile.readLogFile import ReadLogFile
+import numpy as np
 
 
 class MainWindow(QtGui.QMainWindow):
@@ -14,13 +15,14 @@ class MainWindow(QtGui.QMainWindow):
         self.central_widget.addWidget(self.login_widget)
         self.setCentralWidget(self.central_widget)
 
+        self.threshold_box = self.login_widget.threshold_box
+
 
 
     def plotter(self):
         log = 'logs/360 degree scan harbour piles.V4LOG'
         self.file = ReadLogFile(log)
         self.O = OGrid(0.1, 20, 15, 0.5)
-        self.Threshold = 60
         self.img = self.login_widget.img_item
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.updater)
@@ -33,23 +35,44 @@ class MainWindow(QtGui.QMainWindow):
         if msg != 0:
             while msg.type != 2:
                 msg = self.file.readNextMsg()
-            self.O.autoUpdateZhou(msg, self.Threshold)
-            self.img.setImage(self.O.getP().T*255)
+            self.O.autoUpdateZhou(msg, self.threshold_box.value())
+            self.img.setImage(self.O.getP().T)
 
 
 class LoginWidget(QtGui.QWidget):
     def __init__(self, parent=None):
         super(LoginWidget, self).__init__(parent)
-        layout = QtGui.QHBoxLayout() # Main layout
-        self.button = QtGui.QPushButton('Start Plotting')
-        layout.addWidget(self.button) # adding button to main widget
-        self.graphics_view = pg.GraphicsLayoutWidget() # layout for holding graphics object
+        main_layout = QtGui.QHBoxLayout() # Main layout
+        left_layout = QtGui.QVBoxLayout()
+
+
+        graphics_view = pg.GraphicsLayoutWidget() # layout for holding graphics object
         view_box = pg.ViewBox(invertY=True) # making viewbox for the image, inverting y to make it right
+
+        # IMAGE Window
         self.img_item = pg.ImageItem() # image item. the actual plot
+        colormap = pg.ColorMap([0, 0.33, 0.67, 1], np.array([[0.0, 0.0, 0.0, 1.0], [0.0, 1.0, 1.0, 1.0], [1.0, 1.0, 0.0, 1.0], [1.0, 0.0, 0.0, 1.0]]))
+        self.img_item.setLookupTable(colormap.getLookupTable(mode='byte'))
+
+        # Button
+        self.button = QtGui.QPushButton('Start Plotting')
+
+        # Textbox
+        self.threshold_box = QtGui.QSpinBox()
+        self.threshold_box.setMinimum(0)
+        self.threshold_box.setMaximum(255)
+        self.threshold_box.setValue(60)
+
+        # Adding items
+        left_layout.addWidget(self.button)
+        left_layout.addWidget(self.threshold_box)
+
+        main_layout.addLayout(left_layout)
+        main_layout.addWidget(graphics_view)
+
         view_box.addItem(self.img_item)
-        self.graphics_view.addItem(view_box)
-        layout.addWidget(self.graphics_view)
-        self.setLayout(layout)
+        graphics_view.addItem(view_box)
+        self.setLayout(main_layout)
 
 
 if __name__ == '__main__':
