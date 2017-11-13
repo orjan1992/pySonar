c00b00589b9ef446fc8f90ad746b8b39a88b2dcf
Ørjan Grefstad <orjangr@stud.ntnu.no>
2017-11-13 14:36:32 +0100

    Added text boxes for lat long

--- testZhouQt.py
+++ testZhouQt.py
@@ -12,9 +12,11 @@ from messages.moosMsgs import MoosMsgs
 
 LOG_FILENAME = 'ZhouLog.out'
 logging.basicConfig(filename=LOG_FILENAME,
-                    level=logging.INFO,
+                    level=logging.DEBUG,
                     filemode='w',)
 logger = logging.getLogger('ZhouQt')
+logger.disabled = True
+# logging.getLogger('readLogFile.ReadCsvFile').disabled = True
 
 
 class MainWindow(QtGui.QMainWindow):
@@ -32,7 +34,7 @@ class MainWidget(QtGui.QWidget):
         main_layout = QtGui.QHBoxLayout() # Main layout
         left_layout = QtGui.QVBoxLayout()
         right_layout = QtGui.QVBoxLayout()
-        bottom_right_layout = QtGui.QHBoxLayout()
+        bottom_right_layout = QtGui.QGridLayout()
 
         graphics_view = pg.GraphicsLayoutWidget() # layout for holding graphics object
         view_box = pg.ViewBox(invertY=True) # making viewbox for the image, inverting y to make it right
@@ -53,26 +55,52 @@ class MainWidget(QtGui.QWidget):
 
         # Select file
         self.select_file_button = QtGui.QPushButton('Select File')
-        # Select file
+        # From Morse
         self.from_morse_button = QtGui.QPushButton('From Morse')
+        # Check box for continious reading
+        self.cont_reading_checkbox = QtGui.QCheckBox()
+        self.cont_reading_checkbox.setText('Read multiple files')
+        self.cont_reading_checkbox.setChecked(True)
+
 
         # Time box
         self.msg_date = QtGui.QLineEdit()
         self.msg_date.setReadOnly(True)
+
         self.msg_time = QtGui.QLineEdit()
         self.msg_time.setReadOnly(True)
+
+        self.date_text = QtGui.QLineEdit()
+        self.date_text.setReadOnly(True)
+
+        #Lat long rot boxes
+        self.lat_box = QtGui.QLineEdit()
+        self.lat_box.setReadOnly(True)
+
+        self.long_box = QtGui.QLineEdit()
+        self.long_box.setReadOnly(True)
+
+        self.rot_box = QtGui.QLineEdit()
+        self.rot_box.setReadOnly(True)
+
+
 
         # Adding items
         left_layout.addWidget(self.threshold_box)
         left_layout.addWidget(self.start_plotting_button)
         left_layout.addWidget(self.select_file_button)
+        left_layout.addWidget(self.cont_reading_checkbox)
         left_layout.addWidget(self.from_morse_button)
 
         view_box.addItem(self.img_item)
         graphics_view.addItem(view_box)
 
-        bottom_right_layout.addWidget(self.msg_date)
-        bottom_right_layout.addWidget(self.msg_time)
+        bottom_right_layout.addWidget(self.msg_date, 0, 0, 1, 1)
+        bottom_right_layout.addWidget(self.msg_time, 0, 1, 1, 1)
+
+        bottom_right_layout.addWidget(self.lat_box, 1, 0, 1, 1)
+        bottom_right_layout.addWidget(self.long_box, 1, 1, 1, 1)
+        bottom_right_layout.addWidget(self.rot_box, 1, 2, 1, 1)
 
         right_layout.addWidget(graphics_view)
         right_layout.addLayout(bottom_right_layout)
@@ -89,10 +117,11 @@ class MainWidget(QtGui.QWidget):
         #######
         self.first_run = True
         self.pause = True
-        self.fname = 'logs/360 degree scan harbour piles.V4LOG' #inital file name
+        # self.fname = 'logs/360 degree scan harbour piles.V4LOG' #inital file name
         # self.fname = 'logs/UdpHubLog_4001_2017_11_02_09_01_58.csv'
+        self.fname = '/home/orjangr/Repos/pySonar/logs/Sonar Log/UdpHubLog_4001_2017_11_02_09_16_58.csv'
         self.timer = QtCore.QTimer()
-        self.ogrid_conditions = [0.1, 20, 15, 0.5]
+        self.ogrid_conditions = [0.2, 20, 30, 0.5]
         self.old_pos_msg = 0
         self.grid = 0
         self.morse_running = False
@@ -146,9 +175,9 @@ class MainWidget(QtGui.QWidget):
             delta_msg = self.latest_pos_msg_moose - self.old_pos_msg_moose
             if delta_msg.x != 0 or delta_msg.y != 0 or delta_msg.head != 0:
                 self.grid.translational_motion(delta_msg.y, delta_msg.x) # ogrid reference frame
-                # self.grid.rot_motion(delta_psi)
-                print('Transformed grid: delta_x: {}\tdelta_y: {}\tdelta_psi: {} deg'.format(delta_msg.x, delta_msg.y,
-                                                                                             delta_msg.head * 180 / pi))
+                # self.grid.rotate_grid(delta_msg.head)
+                # print('Transformed grid: delta_x: {}\tdelta_y: {}\tdelta_psi: {} deg'.format(delta_msg.x, delta_msg.y,
+                #                                                                              delta_msg.head * 180 / pi))
                 updated = True
             self.old_pos_msg_moose = self.latest_pos_msg_moose
             self.latest_pos_msg_moose = None
@@ -159,7 +188,7 @@ class MainWidget(QtGui.QWidget):
     def plotter_init(self):
         if self.first_run:
             if self.fname.split('.')[-1] == 'csv':
-                self.file = ReadCsvFile(self.fname)
+                self.file = ReadCsvFile(self.fname, cont_reading=self.cont_reading_checkbox.checkState())
             else:
                 self.file = ReadLogFile(self.fname)
             self.grid = OGrid(self.ogrid_conditions[0], self.ogrid_conditions[1], self.ogrid_conditions[2], self.ogrid_conditions[3])
@@ -186,12 +215,18 @@ class MainWidget(QtGui.QWidget):
             elif msg.sensor == 1:
                 if not self.old_pos_msg:
                     self.old_pos_msg = msg
-                # self.grid.rot_motion(msg.head - self.old_pos_msg.head)
-                self.grid.translational_motion(msg.x - self.old_pos_msg.x, msg.y - self.old_pos_msg.y)
-                # print('Delta x: {}\nDeltaY: {}\nDelta psi: {}'.format(msg.x - self.old_pos_msg.x, msg.y - self.old_pos_msg.y, (msg.head - self.old_pos_msg.head)*180/pi))
+                delta_msg = msg - self.old_pos_msg
+                if delta_msg.x != 0 or delta_msg.y != 0 or delta_msg.head != 0:
+                    self.grid.translational_motion(delta_msg.y, delta_msg.x)  # ogrid reference frame
+                    # self.grid.rotate_grid(delta_msg.head*pi/180)
+                    self.lat_box.setText('Lat: {:G}'.format(msg.lat))
+                    self.long_box.setText('Long: {:G}'.format(msg.long))
+                    self.rot_box.setText('Heading: {:G} deg'.format(msg.head))
+                    # self.grid.rotate_grid(delta_msg.head)
+                    logger.debug('Delta x: {}\tDeltaY: {}\tDelta psi: {} deg'.format(delta_msg.x, delta_msg.y, delta_msg.head))
 
-            self.msg_date.setText(msg.date)
-            self.msg_time.setText(msg.time)
+            self.msg_date.setText('Date: {}'.format(msg.date))
+            self.msg_time.setText('Time: {}'.format(msg.time))
 
     def clear_img(self):
         self.img_item.setImage(np.zeros(np.shape(self.img_item.image)))
